f6aa350139656494d156add6bc85503b61e30b91
PHẠM LÝ THANH HIẾU <1612195@student.hcmus.edu.vn>
2019-06-07 00:23:47 +0700

    Test Chuyen Muc Bia Viet

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+var mysql=require('mysql');
+
+var createConnection=()=>{
+    return mysql.createConnection({
+        host:'localhost',
+        port:'3306',
+        user:'root',
+        password:'',
+        database:'baodientu'
+    });
+}
+
+
+module.exports={
+    load:sql=>{
+        return new Promise((resolve, reject)=>{
+            var connection=createConnection();
+            connection.connect();
+            connection.query(sql,(error,resutls,fields)=>{
+                if(error) 
+                    reject(error);
+                else{
+                    resolve(resutls);
+                }
+                connection.end();
+            });
+        });
+    }
+};
